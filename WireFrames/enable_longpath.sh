a110d640783e7ba0855195f1a683b4383f9e9cef
lawshuiyuan <darknesscrawler@hotmail.com>
2025-06-18 16:28:42 +0800

    Revert "merging chatbot files test"
    This reverts commit 86eb79dcf3a75466ebc8341dd7c4a47f50a87391.

--- WireFrames/enable_longpath.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enabling Win32 long paths in Windows Registry..."
-
-powershell.exe -Command '
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -Type DWord
-'
-
-echo "Done. Restart your computer to apply changes."
